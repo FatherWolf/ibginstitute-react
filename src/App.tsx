3819a84a87837798410f94661546cef7b583ae54
FatherWolf <engleau@gmail.com>
2023-06-20 15:26:00 -0400

    added public folder back and made the footer area that stays at the bottom using mui and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
+import Footer from "./components/Footer/Footer";
 
 const App: React.FC = () => {
   return (
     <Router>
+      {/* <Header /> */}
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/" element={<Home />} />
       </Routes>
+    <Footer />
     </Router>
   );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import * as React from 'react';
+import { Box, Typography } from '@mui/material';
 
 const Footer: React.FC = () => {
-  return (
-    <div>
-      <p>Test Footer</p>
-    </div>
-  );
-};
+    return (
+        <Box 
+            sx={{
+                position: 'fixed',
+                bottom: 0,
+                width: '100%',
+                bgcolor: 'white',
+                color: 'black',
+                textAlign: 'center',
+                p: 1 // Padding for aesthetics.
+            }}
+        >
+          <Typography variant="body1">
+            Test Footer
+          </Typography>
+        </Box>
+    );
+}
 
 export default Footer;
