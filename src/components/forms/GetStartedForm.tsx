f460ec9ceb0679b3ec887645dcf6b8f8aa2eafc6
Sara Baqla <missatrox44@gmail.com>
2023-06-21 15:02:50 -0500

    added inital get-started form with dummy data for countries and states

--- src/components/forms/GetStartedForm.tsx
+++ src/components/forms/GetStartedForm.tsx
@@ -1,15 +1,30 @@
 import * as React from 'react';
-import { Box, TextField, Button } from '@mui/material';
+import { Box, TextField, Button, Typography, Grid, FormControl, InputLabel, Select, MenuItem, FormControlLabel, Checkbox } from '@mui/material';
+import { countries, states } from '../../constants/index';
 
 interface FormState {
-  name: string;
+  firstName: string;
+  lastName: string;
   email: string;
+  phone: string;
+  country: string;
+  stateProvince: string;
+  program: string;
+  referralSource: string;
+  acceptTerms: boolean;
 }
 
 const GetStartedForm: React.FC = () => {
   const [values, setValues] = React.useState<FormState>({
-    name: '',
+    firstName: '',
+    lastName: '',
     email: '',
+    phone: '',
+    country: '',
+    stateProvince: '',
+    program: '',
+    referralSource: '',
+    acceptTerms: false,
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,46 +34,180 @@ const GetStartedForm: React.FC = () => {
     });
   };
 
+  const handleSelectChange = (name: keyof typeof values) => (
+    e: React.ChangeEvent<{ name?: string; value: unknown }>
+  ) => {
+    setValues({
+      ...values,
+      [name]: e.target.value,
+    });
+  };
+
+  const handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [event.target.name]: event.target.checked });
+  };
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // Handle the form submission here (like sending it to an API)
-    console.log(values);
+    // Handle the form submission here 
+    console.log('form values: ', values);
   };
 
   return (
     <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-      <TextField
-        variant="outlined"
-        margin="normal"
-        required
-        fullWidth
-        id="name"
-        label="Name"
-        name="name"
-        autoFocus
-        value={values.name}
-        onChange={handleChange}
-      />
-      <TextField
-        variant="outlined"
-        margin="normal"
-        required
-        fullWidth
-        id="email"
-        label="Email Address"
-        name="email"
-        value={values.email}
-        onChange={handleChange}
-      />
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        color="primary"
-        sx={{ mt: 3, mb: 2 }}
-      >
+      <Typography component="h1" variant="h4" sx={{ mt: 2, mb: 2, textAlign: 'center' }}>
         Get Started
-      </Button>
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="firstName"
+            label="First Name"
+            name="firstName"
+            autoFocus
+            value={values.firstName}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="lastName"
+            label="Last Name"
+            name="lastName"
+            autoFocus
+            value={values.lastName}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            fullWidth
+            id="phone"
+            label="Phone Number"
+            name="phone"
+            value={values.phone}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <InputLabel id="country-label">Country</InputLabel>
+            <Select
+              labelId="country-label"
+              id="country"
+              name="country"
+              value={values.country}
+              label="Country"
+              onChange={handleSelectChange('country') as any}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              {countries.map((country) => (
+                <MenuItem key={country.code} value={country.code}>
+                  {country.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <InputLabel id="stateProvince-label">State/Province</InputLabel>
+            <Select
+              labelId="stateProvince-label"
+              id="stateProvince"
+              name="stateProvince"
+              value={values.stateProvince}
+              label="State/Province"
+              onChange={handleSelectChange('stateProvince') as any}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              {states.map((state) => (
+                <MenuItem key={state.code} value={state.code}>
+                  {state.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <InputLabel id="program-label">Program</InputLabel>
+            <Select
+              labelId="program-label"
+              id="program"
+              name="program"
+              value={values.program}
+              label="Program"
+              onChange={handleSelectChange('program') as any}
+            >
+              <MenuItem value={'Fast Track'}>Fast Track</MenuItem>
+              <MenuItem value={'Crash Course'}>Crash Course</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <InputLabel id="referralSource-label">How did you hear about us?</InputLabel>
+            <Select
+              labelId="referralSource-label"
+              id="referralSource"
+              name="referralSource"
+              value={values.referralSource}
+              label="How did you hear about us?"
+              onChange={handleSelectChange('referralSource') as any}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={'Social Media'}>Social Media</MenuItem>
+              <MenuItem value={'IBG Referral'}>IBG Referral</MenuItem>
+              <MenuItem value={'Google/Search Engine'}>Google/Search Engine</MenuItem>
+              <MenuItem value={'Other'}>Other (please specify)</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox color="primary" checked={values.acceptTerms} name="acceptTerms" onChange={handleCheck} />}
+            label="I agree. By checking this box, you are opting-in to receive information from IBG Institute. You also agree to Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida arcu sagittis erat posuere, et efficitur quam vestibulum. Ut iaculis vitae nibh eget congue."
+          />
+        </Grid>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Submit
+        </Button>
+      </Grid>
     </Box>
   );
 }
