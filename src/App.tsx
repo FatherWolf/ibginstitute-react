5d75a224d9cdae1ef82b4aa4b645541d73f5855b
Brad Harris <51719793+HarrisMBrad@users.noreply.github.com>
2023-06-27 15:28:43 -0400

    trying to make the ibg-logo, updated Page Faqs

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,27 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/header/Header";
+import Faqs from "./components/home/faqs/faqs";
+import './App.css'; // Import a CSS file for styling
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    <Footer />
+      <div className="app-container">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/faqs" element={<Faqs />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
 
 export default App;
+
+
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { Box, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { Home } from '@mui/icons-material';
+import { Home, Menu } from '@mui/icons-material';
 
 const Header: React.FC = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   const handleLogoClick = () => {
-    console.log('Logo clicked!');
+    console.log('ibg-logo');
     // Add additional logic or navigation code here
   };
 
   const handleButtonCLick = (buttonName: string) => {
     console.log(`${buttonName} button clicked!`);
     // Add additional logic or navigation code here
+  };
+
+  const handleMobileMenuToggle = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
@@ -24,58 +31,81 @@ const Header: React.FC = () => {
       }}
     >
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
-
+        <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }} onClick={handleLogoClick}>
+          <Home sx={{ fontSize: 32 }} />
+        </Link>
         <Typography variant="h6" component="div" sx={{ ml: 1 }}>
-          
-          <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }} onClick={handleLogoClick}>
-            <Home sx={{ fontSize: 32 }} />
-          </Link>
+          Test Header
         </Typography>
       </Box>
       <Box>
-        <Button
-          component={Link}
-          to="/faqs"
-          variant="contained"
-          color="primary"
-          sx={{ mx: 1 }}
-          onClick={() => handleButtonCLick('FAQs')}
-        >
-          FAQs
-        </Button>
-        <Button
-          component={Link}
-          to="/about"
-          variant="contained"
-          color="primary"
-          sx={{ mx: 1 }}
-          onClick={() => handleButtonCLick('About Us')}
-        >
-          About Us
-        </Button>
-        <Button
-          component={Link}
-          to="/blog"
-          variant="contained"
-          color="primary"
-          sx={{ mx: 1 }}
-          onClick={() => handleButtonCLick('Blog Link')}
-        >
-          Blog Link
-        </Button>
-        <Button
-          component={Link}
-          to="/contact"
-          variant="contained"
-          color="primary"
-          sx={{ mx: 1 }}
-          onClick={() => handleButtonCLick('Contact')}
-        >
-          Contact
-        </Button>
+        {/* Toggle button for mobile menu */}
+        <div className="mobile-menu-toggle" onClick={handleMobileMenuToggle}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Mobile menu */}
+        <div className={`mobile-menu ${isMobileMenuOpen ? 'active' : ''}`}>
+          <Button
+            component={Link}
+            to="/faqs"
+            variant="contained"
+            color="primary"
+            sx={{ mb: 1 }}
+            onClick={() => {
+              handleButtonCLick('FAQs');
+              handleMobileMenuToggle();
+            }}
+          >
+            FAQs
+          </Button>
+          <Button
+            component={Link}
+            to="/about"
+            variant="contained"
+            color="primary"
+            sx={{ mb: 1 }}
+            onClick={() => {
+              handleButtonCLick('About Us');
+              handleMobileMenuToggle();
+            }}
+          >
+            About Us
+          </Button>
+          <Button
+            component={Link}
+            to="/blog"
+            variant="contained"
+            color="primary"
+            sx={{ mb: 1 }}
+            onClick={() => {
+              handleButtonCLick('Blog Link');
+              handleMobileMenuToggle();
+            }}
+          >
+            Blog Link
+          </Button>
+          <Button
+            component={Link}
+            to="/contact"
+            variant="contained"
+            color="primary"
+            sx={{ mb: 1 }}
+            onClick={() => {
+              handleButtonCLick('Contact');
+              handleMobileMenuToggle();
+            }}
+          >
+            Contact
+          </Button>
+        </div>
       </Box>
     </Box>
   );
 };
 
 export default Header;
+
+
