f308b9880ad54f1d369ad60b755343983677d325
Sara Baqla <missatrox44@gmail.com>
2023-07-05 11:22:47 -0500

    added search functionality

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, TextField } from "@mui/material";
 import BlogCard from "../../components/blog/BlogCard";
 import { blogPreviews } from "../../constants/blogPreviews";
 import TagFilter from "../../components/blog/TagFilter";
 
 const Blog: React.FC = () => {
   const [selectedTag, setSelectedTag] = useState<string | null>(null);
+  const [searchText, setSearchText] = useState<string>("");
 
   const handleTagSelect = (tag: string) => {
     if (selectedTag === tag) {
@@ -15,9 +16,19 @@ const Blog: React.FC = () => {
     }
   };
 
-  const filteredBlogs = selectedTag
-    ? blogPreviews.filter((blog) => blog.category === selectedTag)
-    : blogPreviews;
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  const filteredBlogs = blogPreviews.filter((blog) => {
+    const hasMatchingTag = selectedTag ? blog.category === selectedTag : true;
+    const hasMatchingSearch =
+      searchText === "" ||
+      blog.title.toLowerCase().includes(searchText.toLowerCase()) ||
+      blog.summary.toLowerCase().includes(searchText.toLowerCase());
+    return hasMatchingTag && hasMatchingSearch;
+  });
+
 
   const handleReadMore = () => {
     // Logic for redirecting to full blog
@@ -26,10 +37,19 @@ const Blog: React.FC = () => {
   return (
     <Box p={{ xs: 2, md: 7, lg: 10 }}>
       <Typography variant="h2" align="center">
-        Blog Page will go here
+        IBG Institute Tech Blog
       </Typography>
       <Box display="flex" justifyContent="center" my={2}>
         <TagFilter selectedTag={selectedTag} onTagSelect={handleTagSelect} />
+      </Box>
+      <Box my={2}>
+        <TextField
+          label="Search"
+          variant="outlined"
+          fullWidth
+          value={searchText}
+          onChange={handleSearch}
+        />
       </Box>
       <Grid container spacing={4} justifyContent="flex-start">
         {filteredBlogs.map((blog, index) => (
