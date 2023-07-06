0c24a7a0a65464e599bdb3243eeb1fbcb4e813e9
Sara Baqla <missatrox44@gmail.com>
2023-07-06 10:22:03 -0500

    add MiniBlogCard component

---
+++ src/components/blog/MiniBlogCard.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Card, CardContent, Typography, Box, Button } from '@mui/material';
+import CategoryTag from './CategoryTag';
+
+interface MiniBlogCardProps {
+  image: string;
+  category: string;
+  title: string;
+  date: string;
+  selectedTag: string | null;
+  onClick: () => void;
+}
+
+const MiniBlogCard: React.FC<MiniBlogCardProps> = ({
+  image,
+  category,
+  title,
+  date,
+  selectedTag,
+  onClick,
+}) => {
+  return (
+    <Card sx={{ width: '300px', borderRadius: 8, display: 'flex', flexDirection: 'column', marginBottom: 6 }}>
+      <Box sx={{ position: 'relative', width: '100%', height: '200px', overflow: 'hidden' }}>
+        <img
+          src={image}
+          alt="Blog"
+          style={{
+            objectFit: 'cover',
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            top: 0,
+            left: 0
+          }}
+        />
+        <CategoryTag category={category} label={category} selected={category === selectedTag || selectedTag === null} />
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+        <CardContent>
+          <Typography variant="h6" component="h2" align="center" gutterBottom>
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" align="center">
+            {new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
+          </Typography>
+          <Box sx={{ mt: 2, mb: 0, display: 'flex', justifyContent: 'center' }}>
+            <Button variant="contained" onClick={onClick}>
+              Read More
+            </Button>
+          </Box>
+        </CardContent>
+
+      </Box>
+    </Card>
+  );
+};
+
+export default MiniBlogCard;
--- src/pages/blog/BlogDetail.tsx
+++ src/pages/blog/BlogDetail.tsx
@@ -1,13 +1,62 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { blogPreviews } from '../../constants';
+import { Box, Typography, Grid, useMediaQuery, useTheme } from '@mui/material';
+import MiniBlogCard from '../../components/blog/MiniBlogCard';
+import { useNavigate } from "react-router-dom";
 
 const BlogDetail: React.FC = () => {
-  let { blogId } = useParams();
+  let { blogId } = useParams<{ blogId: string }>();
+  const blog = blogPreviews.find((blog) => blog.id === blogId);
+  const navigate = useNavigate();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('sm')); // Detects if we are on small screen size or below (mobile)
+
+  if (!blog) {
+    return <div>Blog not found</div>;
+  }
+
+  const paragraphs = blog.content ? blog.content.split('\n\n') : [];
+
+  const recentBlogs = blogPreviews.filter((item) => item.id !== blog.id).slice(0, 5);
+
+  const handleReadMore = (blogId: string) => {
+    navigate(`/blog/${blogId}`);
+  };
 
   return (
-    <div>
-      <h1>Blog ID: {blogId}</h1>
-    </div>
+    <Box sx={{ pl: [3, 5], pr: 3, pb: 3, pt: 3 }}> {/* Responsive padding */}
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={8}>
+          <img src={blog.image} alt={blog.title} style={{ width: '100%', objectFit: 'cover' }} />
+          <Typography variant="h2" gutterBottom>{blog.title}</Typography>
+          <Typography variant="body1">by {blog.author} on {new Date(blog.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</Typography>
+          {paragraphs.map((paragraph: string, index: number) => (
+            <Typography variant="body1" paragraph key={index}>
+              {paragraph}
+            </Typography>
+          ))}
+        </Grid>
+        {!matches && (
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6" align="center" gutterBottom>Recent articles</Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              {recentBlogs.map((miniBlog) => (
+                <MiniBlogCard
+                  key={miniBlog.id}
+                  image={miniBlog.image}
+                  category={miniBlog.category}
+                  title={miniBlog.title}
+                  date={miniBlog.date}
+                  selectedTag={null}
+                  onClick={() => handleReadMore(miniBlog.id)}
+                />
+              ))}
+            </Box>
+          </Grid>
+        )}
+      </Grid>
+    </Box>
   )
 };
 
