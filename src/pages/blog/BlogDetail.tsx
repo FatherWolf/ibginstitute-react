c4afd7823766036b43f00af2671171d2cd44e0bd
Sara Baqla <missatrox44@gmail.com>
2023-07-06 16:43:06 -0500

    added react-share package and imageCredit and videoEditor keys to blog object

--- src/pages/blog/BlogDetail.tsx
+++ src/pages/blog/BlogDetail.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { EmailShareButton, FacebookShareButton, LinkedinShareButton, TwitterShareButton } from 'react-share';
+import { EmailIcon, FacebookIcon, LinkedinIcon, TwitterIcon } from 'react-share';
 import { blogPreviews } from '../../constants';
 import { Box, Typography, Grid, useMediaQuery, useTheme } from '@mui/material';
 import MiniBlogCard from '../../components/blog/MiniBlogCard';
@@ -10,7 +12,7 @@ const BlogDetail: React.FC = () => {
   const blog = blogPreviews.find((blog) => blog.id === blogId);
   const navigate = useNavigate();
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down('md')); 
+  const matches = useMediaQuery(theme.breakpoints.down('md'));
 
   if (!blog) {
     return <div>Blog not found</div>;
@@ -30,16 +32,48 @@ const BlogDetail: React.FC = () => {
         <Grid item xs={12} md={8}>
           <img src={blog.image} alt={blog.title} style={{ width: '100%', objectFit: 'cover' }} />
           <Typography variant="h2" gutterBottom>{blog.title}</Typography>
-          <Typography variant="body1">by {blog.author} on {new Date(blog.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</Typography>
+          <Typography variant="h6">by {blog.author} on {new Date(blog.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</Typography>
+          <br />
           {paragraphs.map((paragraph: string, index: number) => (
             <Typography variant="body1" paragraph key={index}>
               {paragraph}
             </Typography>
           ))}
+          {blog.videoEditor && (
+            <Typography variant="body2" align="right">
+              Video editing by {blog.videoEditor}
+            </Typography>
+          )}
+          {blog.imageCredit && (
+            <Typography variant="body2" align="right">
+              Featured image by {blog.imageCredit}
+            </Typography>
+          )}
+          <Box display="flex">
+            <Typography marginRight="20px">
+              Share:
+            </Typography>
+            <Box style={{ display: "flex" }}>
+              <EmailShareButton url={window.location.href}>
+                <EmailIcon style={{ marginRight: "20px" }} size={32} round={true} />
+              </EmailShareButton>
+              <TwitterShareButton url={window.location.href}>
+                <TwitterIcon style={{ marginRight: "20px" }} size={32} round={true} />
+              </TwitterShareButton>
+              <FacebookShareButton url={window.location.href}>
+                <FacebookIcon style={{ marginRight: "20px" }} size={32} round={true} />
+              </FacebookShareButton>
+              <LinkedinShareButton url={window.location.href}>
+                <LinkedinIcon style={{ marginRight: "20px" }} size={32} round={true} />
+              </LinkedinShareButton>
+            </Box>
+          </Box>
+
+
         </Grid>
         {!matches && (
           <Grid item xs={12} md={4}>
-            <Typography variant="h6" align="center" gutterBottom>Recent articles</Typography>
+            <Typography variant="h4" align="center" gutterBottom>Recent Blogs</Typography>
             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
               {recentBlogs.map((miniBlog) => (
                 <MiniBlogCard
