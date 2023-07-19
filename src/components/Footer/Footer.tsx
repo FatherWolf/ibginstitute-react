481bb0232c0996b45cff66dccd94f208f9e56659
Brad Harris <51719793+HarrisMBrad@users.noreply.github.com>
2023-07-19 16:17:05 -0400

    added theme from lead, matched social and text to theme for consitancy

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { Box, Typography, IconButton } from '@mui/material';
+import { Box, Typography, IconButton, ThemeProvider, createTheme } from '@mui/material';
 import { Facebook, Twitter, Instagram, LinkedIn } from '@mui/icons-material';
 
 const footerStyles = {
-  backgroundColor: '#C7F9CC',
+  backgroundColor: '#023047',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
@@ -11,48 +11,74 @@ const footerStyles = {
   minHeight: '8vh', // Set a minimum height to occupy the entire viewport vertically
 };
 
+// Extend the CommonColors interface to add the whiteSmoke property
+declare module '@mui/material/styles/createPalette' {
+  interface CommonColors {
+    whiteSmoke: string;
+  }
+}
+
 const Footer: React.FC = () => {
   // Function for opening social media links
   const handleSocialMediaIconClick = (url: string) => {
     window.open(url, "_blank");
   };
 
-  return (
-    <footer>
-      <Box sx={footerStyles}>
-        {/* Box for social media icons */}
-        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-          <IconButton onClick={() => handleSocialMediaIconClick("https://facebook.com")}>
-            <Facebook />
-          </IconButton>
-          <IconButton onClick={() => handleSocialMediaIconClick("https://twitter.com")}>
-            <Twitter />
-          </IconButton>
-          <IconButton onClick={() => handleSocialMediaIconClick("https://instagram.com")}>
-            <Instagram />
-          </IconButton>
-          <IconButton onClick={() => handleSocialMediaIconClick("https://linkedin.com")}>
-            <LinkedIn />
-          </IconButton>
-        </Box>
+  // Material-UI theme with the provided palette
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          primary: {
+            main: '#023047', // Dark blue color
+          },
+          common: {
+            whiteSmoke: '#F5F5F5', // White smoke color
+          },
+        },
+      }),
+    [] // Use an empty dependency array to ensure the theme is created only once
+  );
 
-        {/* Box for address and subscription elements */}
-        <Box>
-          <Typography variant="body1" sx={{
-            textAlign: 'center', fontWeight: 'bold', marginTop: '2rem',
-            marginBottom: '1rem',
-          }}>
-            Charlotte Office <br />
-            333 W. Trade St. Charlotte, NC 20202 <br />
-            IBG Consulting <br />
-            All rights Reserved 2020-2023
-          </Typography>
+  return (
+    <ThemeProvider theme={theme}>
+      <footer>
+        <Box sx={{ ...footerStyles, background: theme.palette.primary.main }}>
+          {/* Box for social media icons */}
+          <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+            <IconButton onClick={() => handleSocialMediaIconClick("https://facebook.com")} style={{ color: 'white' }}>
+              <Facebook />
+            </IconButton>
+            <IconButton onClick={() => handleSocialMediaIconClick("https://twitter.com")} style={{ color: 'white' }}>
+              <Twitter />
+            </IconButton>
+            <IconButton onClick={() => handleSocialMediaIconClick("https://instagram.com")} style={{ color: 'white' }}>
+              <Instagram />
+            </IconButton>
+            <IconButton onClick={() => handleSocialMediaIconClick("https://linkedin.com")} style={{ color: 'white' }}>
+              <LinkedIn />
+            </IconButton>
+          </Box>
+
+          {/* Box for address and subscription elements */}
+          <Box>
+            <Typography variant="body1" sx={{
+              textAlign: 'center', fontWeight: 'bold', marginTop: '2rem',
+              marginBottom: '1rem', color: theme.palette.common.whiteSmoke,
+            }}>
+              Charlotte Office <br />
+              333 W. Trade St. Charlotte, NC 20202 <br />
+              IBG Consulting <br />
+              All rights Reserved 2020-2023
+            </Typography>
+          </Box>
         </Box>
-      </Box>
-    </footer>
+      </footer>
+    </ThemeProvider>
   );
 };
 
 export default Footer;
+
 
 
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,49 +1,67 @@
-import React, { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
-import { Box, Typography, Button, useMediaQuery, ThemeProvider, createTheme, PaletteOptions } from "@mui/material";
-import { Menu, Close } from "@mui/icons-material";
-import logo from "../../assets/institute-icon.png";
+import * as React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Box, Typography, Button, useMediaQuery, ThemeProvider, createTheme } from '@mui/material';
+import { Menu, Close } from '@mui/icons-material';
+import logo from '../../assets/institute-icon.png';
 
 // Extend the PaletteOptions interface to add custom properties
-declare module "@mui/material/styles/createPalette" {
+declare module '@mui/material/styles/createPalette' {
   interface PaletteOptions {
     btn?: string;
   }
 }
 
 const Header: React.FC = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
   const location = useLocation();
-  const isMobileScreen = useMediaQuery("(max-width:733px)");
+  const isMobileScreen = useMediaQuery('(max-width:733px)');
 
   const handleMobileMenuToggle = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (!isMobileScreen) {
       setIsMobileMenuOpen(false);
     }
   }, [isMobileScreen]);
 
-  // Material-UI theme
+  // Material-UI theme with updated color palette
   const theme = createTheme({
     palette: {
       primary: {
-        main: "#80ED99", // Use the primary color here
+        main: '#023047', // Dark blue color
       },
-      btn: "#33577A", // Define the new button color here
+      secondary: {
+        main: '#219EBC', // Teal color
+      },
+      error: {
+        main: '#FB8500', // Orange color for text highlighting
+      },
+      warning: {
+        main: '#FFB703', // Yellow color for accent
+      },
+      info: {
+        main: '#8ECAE6', // Light blue color for accent
+      },
+      success: {
+        main: '#4caf50', // Green color for extra color
+      },
+      common: {
+        white: '#F5F5F5', // Smoke white color
+        whiteSmoke: '#F5F5F5', // White smoke color (Added custom color)
+      },
+      btn: '#33577A', // Define the new button color here
     },
     // Add other theme options as required
     components: {
       MuiButton: {
         styleOverrides: {
           containedPrimary: {
-            backgroundColor: "#33577A", // Use the new button color for primary contained buttons
-            color: "#FFFFFF", // Use white text for the new button color
+            backgroundColor: '#33577A', // Use the new button color for primary contained buttons
           },
           textPrimary: {
-            color: "#000000", // Use black text for the primary text buttons
+            color: '#33577A', // Use the new button color for primary text buttons
           },
         },
       },
@@ -53,22 +71,22 @@ const Header: React.FC = () => {
   // Header styles
   const containerStyle: React.CSSProperties = {
     background: theme.palette.primary.main, // Use the primary color from the theme
-    display: "flex",
-    flexDirection: "column",
-    padding: "1.5rem",
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '1.5rem',
   };
 
   const innerBoxStyle: React.CSSProperties = {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: isMobileScreen ? "row" : "row", // Keep it as row for both mobile and web view
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: isMobileScreen ? 'row' : 'row', // Keep it as row for both mobile and web view
   };
 
   const logoBoxStyle: React.CSSProperties = {
-    display: "flex",
-    alignItems: "center",
-    marginBottom: isMobileScreen ? "1.618rem" : 0,
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: isMobileScreen ? '1.618rem' : 0,
   };
 
   return (
@@ -76,34 +94,34 @@ const Header: React.FC = () => {
       <Box sx={containerStyle}>
         <Box sx={innerBoxStyle}>
           <Box sx={logoBoxStyle}>
-            <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
-              <img src={logo} style={{ width: "2em" }} alt="IBG Institute" />
+            <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+              <img src={logo} style={{ width: '2em' }} alt="IBG Institute" />
             </Link>
             {!isMobileScreen && (
-              <Typography variant="h6" component="div" sx={{ ml: 1 }}>
+              <Typography variant="h6" component="div" sx={{ ml: 1, color: theme.palette.common.whiteSmoke }}>
                 IBG Institute
               </Typography>
             )}
           </Box>
           {isMobileScreen ? (
-            <Menu onClick={handleMobileMenuToggle} sx={{ cursor: "pointer" }} />
+            <Menu onClick={handleMobileMenuToggle} sx={{ cursor: 'pointer' }} />
           ) : (
             <Box // The web view menu is outside the Button component to keep it visible
               sx={{
-                display: "flex",
-                alignItems: "center",
+                display: 'flex',
+                alignItems: 'center',
               }}
             >
-              <Button component={Link} to="/" variant="contained" color="primary" sx={{ marginRight: "1.618rem" }}>
+              <Button component={Link} to="/" variant="contained" color="primary" sx={{ marginRight: '1.618rem' }}>
                 Home
               </Button>
-              <Button component={Link} to="/about" variant="contained" color="primary" sx={{ marginRight: "1.618rem" }}>
+              <Button component={Link} to="/about" variant="contained" color="primary" sx={{ marginRight: '1.618rem' }}>
                 About
               </Button>
-              <Button component={Link} to="/faqs" variant="contained" color="primary" sx={{ marginRight: "1.618rem" }}>
+              <Button component={Link} to="/faqs" variant="contained" color="primary" sx={{ marginRight: '1.618rem' }}>
                 FAQs
               </Button>
-              <Button component={Link} to="/blog" variant="contained" color="primary" sx={{ marginRight: "1.618rem" }}>
+              <Button component={Link} to="/blog" variant="contained" color="primary" sx={{ marginRight: '1.618rem' }}>
                 Blog
               </Button>
               <Button component={Link} to="/contact" variant="contained" color="primary">
@@ -115,34 +133,34 @@ const Header: React.FC = () => {
         {isMobileMenuOpen && (
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-end", // Align "X" icon to the right in mobile view
-              marginTop: "1.618rem",
-              marginBottom: "1.618rem",
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-end', // Align "X" icon to the right in mobile view
+              marginTop: '1.618rem',
+              marginBottom: '1.618rem',
             }}
           >
             {isMobileScreen && ( // Show menu items only in mobile view
               <>
-                <Button component={Link} to="/" variant="contained" color="primary" sx={{ marginBottom: "1.618rem" }}>
+                <Button component={Link} to="/" variant="contained" color="primary" sx={{ marginBottom: '1.618rem' }}>
                   Home
                 </Button>
-                <Button component={Link} to="/about" variant="contained" color="primary" sx={{ marginBottom: "1.618rem" }}>
+                <Button component={Link} to="/about" variant="contained" color="primary" sx={{ marginBottom: '1.618rem' }}>
                   About
                 </Button>
-                <Button component={Link} to="/faqs" variant="contained" color="primary" sx={{ marginBottom: "1.618rem" }}>
+                <Button component={Link} to="/faqs" variant="contained" color="primary" sx={{ marginBottom: '1.618rem' }}>
                   FAQs
                 </Button>
-                <Button component={Link} to="/blog" variant="contained" color="primary" sx={{ marginBottom: "1.618rem" }}>
+                <Button component={Link} to="/blog" variant="contained" color="primary" sx={{ marginBottom: '1.618rem' }}>
                   Blog
                 </Button>
-                <Button component={Link} to="/contact" variant="contained" color="primary" sx={{ marginBottom: "1.618rem" }}>
+                <Button component={Link} to="/contact" variant="contained" color="primary" sx={{ marginBottom: '1.618rem' }}>
                   Contact
                 </Button>
               </>
             )}
             {!isMobileScreen && ( // Hide the icon in web view
-              <Close onClick={handleMobileMenuToggle} sx={{ cursor: "pointer", marginBottom: "1.618rem" }} />
+              <Close onClick={handleMobileMenuToggle} sx={{ cursor: 'pointer', marginBottom: '1.618rem' }} />
             )}
           </Box>
         )}
@@ -152,3 +170,6 @@ const Header: React.FC = () => {
 };
 
 export default Header;
+
+
+
