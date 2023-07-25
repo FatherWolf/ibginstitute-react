26b529d87cd4bcadc0f10173d3d0257273533c68
Brad Harris <51719793+HarrisMBrad@users.noreply.github.com>
2023-07-25 13:44:21 -0400

    loging text to log for heroImg

--- src/pages/home/components/heroImage/HeroImage.tsx
+++ src/pages/home/components/heroImage/HeroImage.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { Box } from '@mui/system';
 import heroImg from '../../../../assets/ibghero.png'
 const HeroImage: React.FC = () => {
+  console.log([`heroImg`], heroImg);
+  // testing to see this img here...
+
+
   return (
-    <Box 
-      sx={{ 
+    <Box
+      sx={{
+        heroImg: 'heroImg',
         height: '33.33vh', // Changed to 33.33% for 1/3rd of the viewport height
         width: '100%',
-        background: heroImg,
-        backgroundSize: 'cover', 
-        backgroundPosition: 'center', 
-        backgroundRepeat: 'no-repeat' 
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
       }}
     >
+
     </Box>
   );
 };
