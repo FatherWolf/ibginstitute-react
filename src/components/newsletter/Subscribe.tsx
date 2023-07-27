83a5533c569af929a8ec8debf575aadce39703b7
Sara Baqla <missatrox44@gmail.com>
2023-07-27 16:25:29 -0500

    added subscribe to newsletter components

---
+++ src/components/newsletter/Subscribe.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Subscribe
+: React.FC = () => {
+  return (
+    <>
+    <iframe src="https://embeds.beehiiv.com/2099d0d8-d5e7-477b-94d8-68bf8e6f2f54" data-test-id="beehiiv-embed" width="480" height="320" style={{borderRadius: "4px", border:2, borderColor:'#e5e7eb', margin: 0, backgroundColor: 'transparent',}}></iframe>
+    </>
+  );
+};
+
+export default Subscribe;
---
+++ src/components/newsletter/SubscribeBtn.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SubscribeBtn
+: React.FC = () => {
+  return (
+    <>
+    <iframe 
+      src="https://embeds.beehiiv.com/2099d0d8-d5e7-477b-94d8-68bf8e6f2f54?slim=true" 
+      data-test-id="beehiiv-embed" 
+      height="52" 
+      style={{margin: 0, borderRadius: 0, backgroundColor: 'transparent'}}
+    />
+    </>
+  );
+};
+
+export default SubscribeBtn;
