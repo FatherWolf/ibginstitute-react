893779e8ef32ca5c094be91f0a1cc82e8253208a
Brad Harris <51719793+HarrisMBrad@users.noreply.github.com>
2023-08-01 16:17:28 -0400

    component displays a toast notification with a message fetched from an API

--- src/components/toasts/SuccessToast.tsx
+++ src/components/toasts/SuccessToast.tsx
@@ -1,14 +1,30 @@
-
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const SuccessToast = ({ message = "Shut Up!" }) => {
-  
- // Using useEffect to show the toast as soon as the component is rendered
+// Assuming 'close' is your own async function that fetches data from an API
+async function close() {
+  // ... implement your function that fetches data and returns it
+  // For example purposes, this function returns a string
+  return "This is a toast message!";
+}
+
+const SuccessToast = () => {
+  const [myMessage, setMyMessage] = useState("");
+
   useEffect(() => {
-    toast(message);
-  }, [message]);
+    const fetchResponse = async () => {
+      const message = await close();
+      setMyMessage(message);
+
+      // Delay the display of toast by 3 seconds
+      setTimeout(() => toast(message), 3000);
+    };
+
+    fetchResponse();
+  }, []);
+
+  console.log(myMessage);
 
   return (
     <div>
