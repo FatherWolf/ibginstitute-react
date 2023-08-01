b8189fe198fe6357eea3c79bc100fdec6bce97e6
Sara Baqla <missatrox44@gmail.com>
2023-08-01 11:37:37 -0500

    removed bullets and text decoration for mobile menu links

--- src/components/header/MobileMenu.tsx
+++ src/components/header/MobileMenu.tsx
@@ -14,37 +14,46 @@ const MobileMenu: React.FC<MobileProps> = ({ onClick }) => {
     background: 'white',
     borderRadius: '8px',
     padding: '1rem',
-    width: '40%', // or your preferred width
-    maxHeight: '80vh', // to prevent the menu from filling the whole screen
-    overflowY: 'auto', // to enable scrolling if the content exceeds the max height
-    boxShadow: '0px 0px 12px rgba(0, 0, 0, 0.25)', // optional shadow for "lifted" effect
+    width: '40%',
+    maxHeight: '80vh',
+    overflowY: 'auto',
+    boxShadow: '0px 0px 12px rgba(0, 0, 0, 0.25)',
+  };
+
+  const linkStyle: React.CSSProperties = {
+    marginBottom: '1.618rem',
+    textDecoration: 'none'
+  };
+
+  const listStyle: React.CSSProperties = {
+    listStyle: 'none' // Removes bullets from the list
   };
 
   return (
     <Box sx={menuStyle}>
-      <ul>
+      <ul style={listStyle}>
         <li>
-          <Typography onClick={onClick} variant="body1" component={Link} to="/" sx={{ marginBottom: '1.618rem', textDecoration: 'none' }}>
+          <Typography onClick={onClick} variant="body1" component={Link} to="/" style={linkStyle}>
             Home
           </Typography>
         </li>
         <li>
-          <Typography onClick={onClick} variant="body1" component={Link} to="/about" sx={{ marginBottom: '1.618rem', textDecoration: 'none' }}>
+          <Typography onClick={onClick} variant="body1" component={Link} to="/about" style={linkStyle}>
             About
           </Typography>
         </li>
         <li>
-          <Typography onClick={onClick} variant="body1" component={Link} to="/faqs" sx={{ marginBottom: '1.618rem', textDecoration: 'none' }}>
+          <Typography onClick={onClick} variant="body1" component={Link} to="/faqs" style={linkStyle}>
             FAQs
           </Typography>
         </li>
         <li>
-          <Typography onClick={onClick} variant="body1" component={Link} to="/blog" sx={{ marginBottom: '1.618rem', textDecoration: 'none' }}>
+          <Typography onClick={onClick} variant="body1" component={Link} to="/blog" style={linkStyle}>
             Blog
           </Typography>
         </li>
         <li>
-          <Typography onClick={onClick} variant="body1" component={Link} to="/contact" sx={{ marginBottom: '1.618rem', textDecoration: 'none' }}>
+          <Typography onClick={onClick} variant="body1" component={Link} to="/contact" style={linkStyle}>
             Contact
           </Typography>
         </li>
@@ -52,6 +61,5 @@ const MobileMenu: React.FC<MobileProps> = ({ onClick }) => {
     </Box>
   );
 };
-
 
 export default MobileMenu;
