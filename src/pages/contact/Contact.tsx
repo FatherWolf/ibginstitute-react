162a77f3cf52b1367548bed3fd8a9aabc8496235
Sara Baqla <missatrox44@gmail.com>
2023-08-02 13:19:32 -0500

    added color to contact page

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -3,27 +3,34 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import EmailIcon from "@mui/icons-material/Email";
 import PhoneIcon from "@mui/icons-material/Phone";
 import ContactForm from "../../components/forms/ContactForm";
+import theme from "../../theme";
 
 const Contact: React.FC = () => {
   return (
-    <Box>
+    <Box
+      sx={{
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.info.main,
+        padding: 3
+      }}
+    >
       <Typography variant="h3" align="center" marginTop={4} marginBottom={4}>
         Drop Us A Line!
       </Typography>
       <Grid container justifyContent="center" alignItems="center" spacing={3}>
         <Grid item xs={12} sm={4}>
-          <Typography variant="h6" align="center" display="flex" alignItems="center" justifyContent="center">
-            <LocationOnIcon style={{ marginRight: '8px' }} /> Charlotte, NC
+          <Typography variant="h6" align="center" display="flex" alignItems="center" justifyContent="center" sx={{ color: theme.palette.info.main }}>
+            <LocationOnIcon style={{ marginRight: '8px', color: theme.palette.info.main }} /> Charlotte, NC
           </Typography>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <Typography variant="h6" align="center" display="flex" alignItems="center" justifyContent="center">
-            <EmailIcon style={{ marginRight: '8px' }} />info@ibginstitute.com
+          <Typography variant="h6" align="center" display="flex" alignItems="center" justifyContent="center" sx={{ color: theme.palette.info.main }}>
+            <EmailIcon style={{ marginRight: '8px', color: theme.palette.info.main }} />info@ibginstitute.com
           </Typography>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <Typography variant="h6" align="center" display="flex" alignItems="center" justifyContent="center">
-            <PhoneIcon style={{ marginRight: '8px' }} /> (980) 296-2699
+          <Typography variant="h6" align="center" display="flex" alignItems="center" justifyContent="center" sx={{ color: theme.palette.info.main }}>
+            <PhoneIcon style={{ marginRight: '8px', color: theme.palette.info.main }} /> (980) 296-2699
           </Typography>
         </Grid>
       </Grid>
@@ -34,7 +41,7 @@ const Contact: React.FC = () => {
         marginBottom={2}
         sx={{
           padding: { xs: 2, sm: 0 },
-          margin: { xs: 2, sm: 'auto' }
+          margin: { xs: 2, sm: 'auto' },
         }}
       >
         <Typography variant="body1" align="center">
