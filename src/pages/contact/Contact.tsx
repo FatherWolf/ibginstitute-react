2666d513842ca5b4aa14f8e86885e598df25f2d9
Sara Baqla <missatrox44@gmail.com>
2023-08-02 12:21:22 -0500

    updated Contact page styles

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,18 +1,40 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIcon from "@mui/icons-material/Phone";
 import ContactForm from "../../components/forms/ContactForm";
 
 const Contact: React.FC = () => {
-
-
   return (
     <Box>
-      <Typography variant="h2" align="center">
+      <Typography variant="h3" align="center" marginTop={4} marginBottom={4}>
         Drop Us A Line!
       </Typography>
-      <Typography variant="body1" align="center">
-        Our chatbot does its best, but we understand that some questions need a human touch. Drop us a message using the form below, and we'll assist you promptly.
-      </Typography>
-      <ContactForm />
+      <Grid container justifyContent="center" alignItems="center" spacing={3}>
+        <Grid item xs={12} sm={4}>
+          <Typography variant="h6" align="center">
+            <LocationOnIcon /> Charlotte, NC
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Typography variant="h6" align="center">
+            <EmailIcon /> info@ibginstitute.com
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Typography variant="h6" align="center">
+            <PhoneIcon /> (980) 296-2699
+          </Typography>
+        </Grid>
+      </Grid>
+      <Box maxWidth={600} margin="auto" marginTop={2} marginBottom={2}>
+        <Typography variant="body1" align="center">
+          Our chatbot does its best, but we understand that some questions need a human touch. Drop us a message using the form below, and we'll assist you promptly.
+        </Typography>
+      </Box>
+      <Box padding={3}>
+        <ContactForm />
+      </Box>
     </Box>
   );
 };
