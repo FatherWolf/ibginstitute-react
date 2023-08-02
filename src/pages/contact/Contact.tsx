ce0b9ef3eb396a6195a257db0bd30c0137aaf6ab
Sara Baqla <missatrox44@gmail.com>
2023-08-02 10:55:53 -0500

    added chatbot to index.html

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,19 +1,21 @@
-import React from "react";
 import { Box, Typography } from "@mui/material";
 import ContactForm from "../../components/forms/ContactForm";
 
 const Contact: React.FC = () => {
+
+
   return (
     <Box>
       <Typography variant="h2" align="center">
         Drop Us A Line!
       </Typography>
       <Typography variant="body1" align="center">
-      Our chatbot does its best, but we understand that some questions need a human touch. Drop us a message using the form below, and we'll assist you promptly.
+        Our chatbot does its best, but we understand that some questions need a human touch. Drop us a message using the form below, and we'll assist you promptly.
       </Typography>
       <ContactForm />
+      <div id="chatbot"></div> Div for the chatbot script
     </Box>
   );
-}
+};
 
 export default Contact;
