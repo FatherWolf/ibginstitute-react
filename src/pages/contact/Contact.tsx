4f9f1c4d0f6859e740813ea5329284f49f8f52fa
Sara Baqla <missatrox44@gmail.com>
2023-08-03 13:05:41 -0500

    location, email, and phone are functional when user clicks on them

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -6,6 +6,11 @@ import ContactForm from "../../components/forms/ContactForm";
 import theme from "../../theme";
 
 const Contact: React.FC = () => {
+  const locationLink = "https://www.google.com/maps/search/?api=1&query=333+W+Trade+St+Charlotte,+NC+20202";
+  const emailLink = "mailto:info@ibginstitute.com";
+  const phoneLink = "tel:+19802962699";
+
+
   return (
     <Box
       sx={{
@@ -19,18 +24,28 @@ const Contact: React.FC = () => {
       </Typography>
       <Grid container justifyContent="center" alignItems="center" spacing={3}>
         <Grid item xs={12} sm={4}>
-          <Typography variant="h6" align="center" display="flex" alignItems="center" justifyContent="center" sx={{ color: theme.palette.warning.main }}>
-            <LocationOnIcon style={{ marginRight: '8px', color: theme.palette.warning.main }} /> Charlotte, NC
+          <Typography variant="h6" align="center" display="flex" alignItems="center" justifyContent="center">
+            <LocationOnIcon style={{ marginRight: '8px', color: theme.palette.warning.main }} />
+            <a target="_blank" href={locationLink} style={{ display: 'block', textAlign: 'center', color: theme.palette.warning.main, textDecoration: 'none' }} rel="noreferrer">
+              Charlotte, NC
+            </a>
           </Typography>
         </Grid>
         <Grid item xs={12} sm={4}>
           <Typography variant="h6" align="center" display="flex" alignItems="center" justifyContent="center" sx={{ color: theme.palette.success.main }}>
-            <EmailIcon style={{ marginRight: '8px', color: theme.palette.success.main }} />info@ibginstitute.com
+            <EmailIcon style={{ marginRight: '8px', color: theme.palette.success.main }} />
+            <a href={emailLink} style={{ display: 'block', textAlign: 'center', color: theme.palette.success.main, textDecoration: 'none' }} rel="noreferrer">
+              info@ibginstitute.com
+            </a>
           </Typography>
         </Grid>
         <Grid item xs={12} sm={4}>
           <Typography variant="h6" align="center" display="flex" alignItems="center" justifyContent="center" sx={{ color: theme.palette.error.main }}>
-            <PhoneIcon style={{ marginRight: '8px', color: theme.palette.error.main }} /> (980) 296-2699
+            <PhoneIcon style={{ marginRight: '8px', color: theme.palette.error.main }} />
+            <a href={phoneLink} style={{ display: 'block', textAlign: 'center', color: theme.palette.error.main, textDecoration: 'none' }} rel="noreferrer">
+              (980) 296-2699
+            </a>
+
           </Typography>
         </Grid>
       </Grid>
