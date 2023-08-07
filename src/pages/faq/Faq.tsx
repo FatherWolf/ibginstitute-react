cc63f90a238000f2d25742ec9396640f7f8af04c
Sara Baqla <missatrox44@gmail.com>
2023-08-07 15:42:48 -0500

    created two columns on FAQ page

--- src/pages/faq/Faq.tsx
+++ src/pages/faq/Faq.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Accordion, AccordionSummary, AccordionDetails, Box } from '@mui/material';
+import { Typography, Accordion, AccordionSummary, AccordionDetails, Box, Grid } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useTheme } from '@mui/material/styles';
 
@@ -9,7 +9,6 @@ const Faq: React.FC = () => {
   return (
     <Box
       sx={{
-        width: '100%',
         padding: theme.spacing(2),
         backgroundColor: theme.palette.primary.main,
         color: theme.palette.common.white,
@@ -18,39 +17,64 @@ const Faq: React.FC = () => {
       <Typography variant="h2" align="center" gutterBottom sx={{ color: theme.palette.secondary.main }}>
         FAQs
       </Typography>
-
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-          sx={{ color: theme.palette.common.black }}
-        >
-          <Typography>Question 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography sx={{ color: theme.palette.common.black }}>
-            Answer to question 1...
+      
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h4" align="center" gutterBottom sx={{ color: theme.palette.secondary.main }}>
+            Purchases & Refunds
           </Typography>
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-          sx={{ color: theme.palette.common.black }}
-        >
-          <Typography>Question 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography sx={{ color: theme.palette.common.black }}>
-            Answer to question 2...
+          {Array.from({ length: 3 }).map((_, i) => (
+            <Accordion key={i}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls={`panel${i + 1}a-content`}
+                id={`panel${i + 1}a-header`}
+                sx={{ color: theme.palette.common.black }}
+              >
+                <Typography>{`Question ${i + 1}`}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography sx={{ color: theme.palette.common.black }}>
+                  {`Answer to question ${i + 1}...`}
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          ))}
+        </Grid>
+        
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h4" align="center" gutterBottom sx={{ color: theme.palette.secondary.main }}>
+            Taking the Course
           </Typography>
-        </AccordionDetails>
-      </Accordion>
+          {Array.from({ length: 11 }).map((_, i) => (
+            <Accordion key={i}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls={`panel${i + 4}a-content`}
+                id={`panel${i + 4}a-header`}
+                sx={{ color: theme.palette.common.black }}
+              >
+                <Typography>{`Question ${i + 4}`}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography sx={{ color: theme.palette.common.black }}>
+                  {`Answer to question ${i + 4}...`}
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          ))}
+        </Grid>
+      </Grid>
 
+      <Typography 
+        variant="h6" 
+        align="center" 
+        sx={{ 
+          color: theme.palette.secondary.main,
+          marginTop: theme.spacing(3)
+        }}>
+        If you have any additional questions, please feel free to contact us!
+      </Typography>
     </Box>
   );
 }
