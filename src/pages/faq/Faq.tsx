3a721f6f492a041b6d2ed71e0934672e9f57378b
Sara Baqla <missatrox44@gmail.com>
2023-08-07 16:12:42 -0500

    added lighter color to palette, added styles to FAQ

--- src/pages/faq/Faq.tsx
+++ src/pages/faq/Faq.tsx
@@ -3,6 +3,7 @@ import { Typography, Accordion, AccordionSummary, AccordionDetails, Box, Grid } 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useTheme } from '@mui/material/styles';
 import { faqData } from '../../constants';
+import { Link } from "react-router-dom"
 
 const Faq: React.FC = () => {
   const theme = useTheme();
@@ -10,7 +11,8 @@ const Faq: React.FC = () => {
   return (
     <Box
       sx={{
-        padding: theme.spacing(2),
+        py: theme.spacing(2),
+        px: theme.spacing(6),
         backgroundColor: theme.palette.primary.main,
         color: theme.palette.common.white,
       }}
@@ -26,17 +28,26 @@ const Faq: React.FC = () => {
               {category.category}
             </Typography>
             {category.faqs.map((faq, j) => (
-              <Accordion key={j}>
+              <Accordion key={j} sx={{ transition: '0.3s', boxShadow: '1px 1px 10px rgba(0,0,0,0.1)' }}>
                 <AccordionSummary
                   expandIcon={<ExpandMoreIcon />}
                   aria-controls={`panel${j + 1}a-content`}
                   id={`panel${j + 1}a-header`}
-                  sx={{ color: theme.palette.common.black }}
+                  sx={{
+                    backgroundColor: theme.palette.primary.light,
+                    color: theme.palette.common.white,
+                    padding: theme.spacing(2),
+                  }}
                 >
                   <Typography>{faq.question}</Typography>
                 </AccordionSummary>
-                <AccordionDetails>
-                  <Typography sx={{ color: theme.palette.common.black }}>
+                <AccordionDetails
+                  sx={{
+                    backgroundColor: theme.palette.primary.light,
+                    padding: theme.spacing(2),
+                  }}
+                >
+                  <Typography sx={{ color: theme.palette.common.white }}>
                     {faq.answer}
                   </Typography>
                 </AccordionDetails>
@@ -52,8 +63,17 @@ const Faq: React.FC = () => {
         sx={{
           color: theme.palette.secondary.main,
           marginTop: theme.spacing(3)
-        }}>
-        If you have any additional questions, please feel free to contact us!
+        }}
+      >
+        If you have any additional questions, please feel free to
+        {" "}
+        <Link
+          to="/contact"
+          style={{ textDecoration: 'none', color: theme.palette.warning.main }}
+        >
+          contact us
+        </Link>
+        !
       </Typography>
     </Box>
   );
