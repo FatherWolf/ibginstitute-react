c9af4ebfe50e97e86d7b321aa10a47b4500c5a1e
Sara Baqla <missatrox44@gmail.com>
2023-08-07 15:50:59 -0500

    added faqData

---
+++ src/constants/faqData.ts
@@ -0,0 +1,68 @@
+export const faqData = [
+  {
+    "category": "Purchases & Refunds",
+    "faqs": [
+      {
+        "question": "May I have a refund for the course?",
+        "answer": "Yes, you may have a refund in the first week of taking the course. After that we would no longer be able to refund you."
+      },
+      {
+        "question": "Is there a payment plan?",
+        "answer": "Yes, IBG Institute offers several options for payment plans to make the training accessible to a wider audience. If you require special assistance or financial aid, please contact enrollment@ibginstittue.com with your concern."
+      },
+      {
+        "question": "Are there any discounts available for the courses?",
+        "answer": "There are no discounts available for our courses, as it is already offered at a significantly reduced price compared to other companies' rates. IBG Institute aims to make its training accessible and cost-effective for all."
+      },
+    ]
+  },
+  {
+    "category": "Taking the Course",
+    "faqs": [
+      {
+        "question": "Do I need any prior knowledge?",
+        "answer": "While it is helpful. It is not needed to start our course."
+      },
+      {
+        "question": "How do I access the course?",
+        "answer": "We currently run our courses on Google Classroom. Once payment has been received for the class you should be redirected into Google Classroom."
+      },
+      {
+        "question": "How often are live lectures?",
+        "answer": "Once a week."
+      },
+      {
+        "question": "Am I required to do all the work?",
+        "answer": "Yes, it is mandatory to complete all the assigned work during the training. While some tasks may be more manageable than others, perseverance is key to success."
+      },
+      {
+        "question": "Why do student join online programs at IBG Institute?",
+        "answer": "Students join online programs at IBG Institute for a variety of reasons, such as upgrading their skills, starting a career change, taking the next step in formal education, or easily transitioning before entering a full-time academic program. IBG Institute's online courses provide a flexible way to learn without disrupting daily life."
+      },
+      {
+        "question": "How do I access the course materials after signing up?",
+        "answer": "You will be plugged in directly into our google classroom. We have a discord set up for our students. New sessions of the class will be plugged into our IBG Institute Slack Channel for more direct access to our instructors."
+      },
+      {
+        "question": "Seems too good to be true!",
+        "answer": "IBG Institute emphasizes that hard work is required to complete the program successfully. The Institute aims to provide practical skills and resources to help students secure jobs in a cost-effective manner. Over 70% of their trainees have found employment, and the Institute commits to supporting students until they land a job, either with IBG or another company."
+      },
+      {
+        "question": "Am I guaranteed a job if I do this?",
+        "answer": "While IBG Institute strives to assist students in securing employment, job placement is not guaranteed. The Institute provides access to various resources, and success depends on the student's commitment to follow the training and after-training guidelines."
+      },
+      {
+        "question": "What if I need a visa sponsorship?",
+        "answer": "For students who complete the FAST TRACK program and meet all requirements, IBG Institute may sponsor their visa and assist in securing a job. Visa sponsorship is only considered for the FAST TRACK program."
+      },
+      {
+        "question": "What happens when I finish the program?",
+        "answer": "Upon completion of the program, students can begin interview preparation, and IBG Institute will actively work to help them secure a job, either within IBG Software or with another company. Alternatively, students may receive additional no-cost training to advance their skills. They may also be invited to participate in the apprentice Tech Stars program or the 6-12 month advanced training programs.."
+      },
+      {
+        "question": "Do you have an apprentice program?",
+        "answer": "Yes, once students complete the FAST TRACK program, they may be invited to join the IBG Institute's apprentice program. This provides an opportunity for real project experience and hands-on learning."
+      },
+    ]
+  }
+]
