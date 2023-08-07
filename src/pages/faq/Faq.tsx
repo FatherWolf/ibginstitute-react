ee47ece4fcc425faec0d68a6ccf8e474d6bb73d2
Sara Baqla <missatrox44@gmail.com>
2023-08-07 15:56:11 -0500

    rendering faqs

--- src/pages/faq/Faq.tsx
+++ src/pages/faq/Faq.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Typography, Accordion, AccordionSummary, AccordionDetails, Box, Grid } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useTheme } from '@mui/material/styles';
+import { faqData } from '../../constants';
 
 const Faq: React.FC = () => {
   const theme = useTheme();
@@ -17,59 +18,38 @@ const Faq: React.FC = () => {
       <Typography variant="h2" align="center" gutterBottom sx={{ color: theme.palette.secondary.main }}>
         FAQs
       </Typography>
-      
+
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h4" align="center" gutterBottom sx={{ color: theme.palette.secondary.main }}>
-            Purchases & Refunds
-          </Typography>
-          {Array.from({ length: 3 }).map((_, i) => (
-            <Accordion key={i}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls={`panel${i + 1}a-content`}
-                id={`panel${i + 1}a-header`}
-                sx={{ color: theme.palette.common.black }}
-              >
-                <Typography>{`Question ${i + 1}`}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography sx={{ color: theme.palette.common.black }}>
-                  {`Answer to question ${i + 1}...`}
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-          ))}
-        </Grid>
-        
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h4" align="center" gutterBottom sx={{ color: theme.palette.secondary.main }}>
-            Taking the Course
-          </Typography>
-          {Array.from({ length: 11 }).map((_, i) => (
-            <Accordion key={i}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls={`panel${i + 4}a-content`}
-                id={`panel${i + 4}a-header`}
-                sx={{ color: theme.palette.common.black }}
-              >
-                <Typography>{`Question ${i + 4}`}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography sx={{ color: theme.palette.common.black }}>
-                  {`Answer to question ${i + 4}...`}
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-          ))}
-        </Grid>
+        {faqData.map((category, i) => (
+          <Grid item xs={12} sm={6} key={i}>
+            <Typography variant="h4" align="center" gutterBottom sx={{ color: theme.palette.secondary.main }}>
+              {category.category}
+            </Typography>
+            {category.faqs.map((faq, j) => (
+              <Accordion key={j}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls={`panel${j + 1}a-content`}
+                  id={`panel${j + 1}a-header`}
+                  sx={{ color: theme.palette.common.black }}
+                >
+                  <Typography>{faq.question}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography sx={{ color: theme.palette.common.black }}>
+                    {faq.answer}
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            ))}
+          </Grid>
+        ))}
       </Grid>
 
-      <Typography 
-        variant="h6" 
-        align="center" 
-        sx={{ 
+      <Typography
+        variant="h6"
+        align="center"
+        sx={{
           color: theme.palette.secondary.main,
           marginTop: theme.spacing(3)
         }}>
